592c63843e803c124bf26cc739c13be70aad010a
TahelNadav <tahelnadav@gmail.com>
2020-09-16 00:48:29 +0300

    myfirst

---
+++ Medicine/BE/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Person
+    {
+        
+        public int Id { get; set; }
+        [DisplayName("תעודת זהות")]
+        public int SocialSecurityNumber { get; set; }
+        [DisplayName("שם פרטי")]
+        public string FirstName { get; set; }
+        [DisplayName("שם משפחה")]
+        public string LastName { get; set; }
+        
+    }
+}
